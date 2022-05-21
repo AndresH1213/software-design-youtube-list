b356b8ef022428cdb0ebcd42e1edf6ef125958be
Andres Hernandez <edanhebla1213@hotmail.com>
2022-05-20 23:53:48 -0500

    strategy pattern concrete process_ticket strategy

---
+++ strategy-pattern/strategy-after.py
@@ -0,0 +1,77 @@
+import string
+import random
+from typing import List
+from abc import ABC, abstractmethod
+
+
+def generate_id(length=8):
+    # helper function for generating an id
+    return ''.join(random.choices(string.ascii_uppercase, k=length))
+
+class SupportTicket:
+
+    def __init__(self, customer, issue):
+        self.id = generate_id()
+        self.customer = customer
+        self.issue = issue
+
+
+class TicketOrderingStrategy(ABC):
+    @abstractmethod
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        pass
+
+"""Now we can create classes for each of this specific ordering types"""
+class FIFOOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        return list.copy()
+
+class FILOOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        list_copy = list.copy()
+        list_copy.reverse()
+        return list_copy
+
+class RandomOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        list_copy = list.copy()
+        random.shuffle(list_copy)
+        return list_copy
+
+class CustomerSupport:
+
+    tickets: List[SupportTicket] = []
+
+    def create_ticket(self, customer, issue):
+        self.tickets.append(SupportTicket(customer, issue))
+
+    def process_tickets(self, processing_strategy):
+        #create the ordered list
+        ticket_list = processing_strategy.create_ordering(self.tickets)
+
+        # if it's empty, don't do anything
+        if len(ticket_list) == 0:
+            print("There are no tickets to process. Well done!")
+            return
+
+        for ticket in ticket_list:
+            self.process_ticket(ticket)
+
+    def process_ticket(self, ticket: SupportTicket):
+        print("==================================")
+        print(f"Processing ticket id: {ticket.id}")
+        print(f"Customer: {ticket.customer}")
+        print(f"Issue: {ticket.issue}")
+        print("==================================")
+
+
+# create the application
+app = CustomerSupport()
+
+# register a few tickets
+app.create_ticket("John Smith", "My computer makes strange sounds!")
+app.create_ticket("Linus Sebastian", "I can't upload any videos, please help.")
+app.create_ticket("Arjan Egges", "VSCode doesn't automatically solve my bugs.")
+
+# process the tickets
+app.process_tickets(RandomOrderingStrategy())
