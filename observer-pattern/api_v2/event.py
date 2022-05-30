dbd6832bff9e9b3ea55d49fca1516d721bbff2d7
Andres Hernandez <edanhebla1213@hotmail.com>
2022-05-29 23:57:16 -0500

    event implementation

--- observer-pattern/api_v2/event.py
+++ observer-pattern/api_v2/event.py
@@ -1,9 +1,12 @@
+"""simple event implementation"""
 subscribers = dict()
+
 
 def subscribe(event_type: str, fn):
     if not event_type in subscribers:
         subscribers[event_type] = []
     subscribers[event_type].append(fn)
+
 
 def post_event(event_type: str, data):
     if not event_type in subscribers:
