137782fb2708e7c1a6a55be0df9d74cb0d960b6e
Andres Hernandez <edanhebla1213@hotmail.com>
2022-08-12 22:38:39 -0500

    dependency inversion implement

---
+++ SOLID-principles/dependency-inversion.py
@@ -0,0 +1,94 @@
+"""
+I want our classes to depend on abstractions and
+not in concrete subclasses
+"""
+from abc import ABC, abstractmethod
+
+class Order:
+
+    def __init__(self):
+        self.items = []
+        self.quantities = []
+        self.prices = []
+        self.status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+class Authorizer(ABC):
+    @abstractmethod
+    def is_authorized(self) -> bool:
+        pass
+
+class SMSAuth(Authorizer):
+    authorized = False
+    def verify(self, code):
+        print(f"Verifying code {code}")
+        self.authorized = True
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+class NotARobot(Authorizer):
+    authorized = False
+    def not_a_robot(self):
+        print("Are you a robot? Naaa...")
+        self.authorized = True
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+class PaymentProcessor(ABC):
+    @abstractmethod
+    def pay(self, order):
+        pass
+
+
+class DebitPaymentProcessor(PaymentProcessor):
+    def __init__(self, security_code, authorizer: Authorizer):
+        self.authorizer = authorizer
+        self.security_code = security_code
+
+    def pay(self,order):
+        if not self.authorizer.is_authorized():
+            raise Exception("Not authorized")
+        print("Processing debit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
+
+class CreditPaymentProcessor(PaymentProcessor):
+    def __init__(self, security_code):
+        self.security_code = security_code
+
+    def pay(self,order):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {self.security_code}")
+        order.status = "paid"
+
+class PaypalPaymentProcessor(PaymentProcessor):
+    def __init__(self, email_address, authorizer: Authorizer):
+        self.authorizer = authorizer
+        self.email_address = email_address
+    def pay(self, order):
+        if not self.authorizer.is_authorized():
+            raise Exception("Not authorized")
+        print("Processing paypal payment type")
+        print(f"Verifying email address: {self.email_address}")
+        order.status = "paid"
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+
+print(order.total_price())
+authorizer = SMSAuth()
+processor = DebitPaymentProcessor("212313", authorizer)
+authorizer.verify(23109)
+processor.pay(order)
